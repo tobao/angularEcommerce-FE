aefbf42258ccb8c5fec916f290a1fba93e7afd66
BaoTo <tobao97@gmail.com>
2025-04-01 16:59:40 +0700

    Create Product class and Product Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
