afeb21c9c3249dc73c61c8c74d8253f3959c9cee
BaoTo <tobao97@gmail.com>
2025-04-13 21:38:32 +0700

    Pagination Selecting Page Size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -18,7 +18,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -93,6 +93,12 @@ export class ProductListComponent implements OnInit {
                                                   this.theTotalElements = data.page.totalElements;
                                                 }
                                                );
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 
 }
