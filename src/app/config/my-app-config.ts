486c6592e22652d1cb7cb1008f3afe3df85c4699
BaoTo <tobao97@gmail.com>
2025-06-20 10:57:57 +0700

    Secure Communication with HTTPS

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaothcyv6PVhH7MH5d7',
     issuer: 'https://dev-12702799.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: false,
